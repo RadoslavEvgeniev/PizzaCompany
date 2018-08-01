7fc4b56491632b879f7f9ae4713520ca97506ec9
Radoslav <r.evgeniev.ivanov@gmail.com>
2018-08-01 21:57:16 +0300

    Added
    Base and Home controllers, reset.css, base layout with head and navbar, index views
    
    Changed
    
    style.css: navbar background color,

---
+++ src/main/java/pizzaco/web/controllers/BaseController.java
@@ -0,0 +1,35 @@
+package pizzaco.web.controllers;
+
+import org.springframework.web.servlet.ModelAndView;
+
+public abstract class BaseController {
+
+    protected ModelAndView view(String view, String objectName, Object object) {
+        ModelAndView modelAndView = new ModelAndView();
+
+        modelAndView.addObject("view", view);
+        modelAndView.addObject(objectName, object);
+
+        modelAndView.setViewName("fragments/base-layout");
+
+        return modelAndView;
+    }
+
+    protected ModelAndView view(String view) {
+        ModelAndView modelAndView = new ModelAndView();
+
+        modelAndView.addObject("view", view);
+
+        modelAndView.setViewName("fragments/base-layout");
+
+        return modelAndView;
+    }
+
+    protected ModelAndView redirect(String url) {
+        ModelAndView modelAndView = new ModelAndView();
+
+        modelAndView.setViewName("redirect:" + url);
+
+        return modelAndView;
+    }
+}
