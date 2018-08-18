9fa7ce5d7a2cc03d414f874ecab2dbc3a3954894
Radoslav <r.evgeniev.ivanov@gmail.com>
2018-08-18 16:35:22 +0300

    Added UserRoleServiceModel

---
+++ src/main/java/pizzaco/domain/models/service/UserRoleServiceModel.java
@@ -0,0 +1,17 @@
+package pizzaco.domain.models.service;
+
+public class UserRoleServiceModel {
+
+    private String authority;
+
+    public UserRoleServiceModel() {
+    }
+
+    public String getAuthority() {
+        return this.authority;
+    }
+
+    public void setAuthority(String authority) {
+        this.authority = authority;
+    }
+}
