243b856d584f21265141f12c57a466da0fc82e4a
Radoslav <r.evgeniev.ivanov@gmail.com>
2018-08-18 16:57:47 +0300

    Added
    Log entity

---
+++ src/main/java/pizzaco/domain/entities/Log.java
@@ -0,0 +1,44 @@
+package pizzaco.domain.entities;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "logs")
+public class Log extends BaseEntity {
+
+    private LocalDateTime dateTime;
+    private User user;
+    private String event;
+
+    public Log() {
+    }
+
+    @Column(name = "date_time", nullable = false, updatable = false)
+    public LocalDateTime getDateTime() {
+        return this.dateTime;
+    }
+
+    public void setDateTime(LocalDateTime dateTime) {
+        this.dateTime = dateTime;
+    }
+
+    @ManyToOne(targetEntity = User.class)
+    @JoinColumn(name = "user_id")
+    public User getUser() {
+        return this.user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    @Column(name = "event", columnDefinition = "text")
+    public String getEvent() {
+        return this.event;
+    }
+
+    public void setEvent(String event) {
+        this.event = event;
+    }
+}
