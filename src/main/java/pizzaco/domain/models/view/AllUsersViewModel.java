8c35f1d6da8cf45c9f69f3ad3f938b1e5261e6ec
Radoslav <r.evgeniev.ivanov@gmail.com>
2018-08-18 16:35:42 +0300

    Added AllUsersViewModel

---
+++ src/main/java/pizzaco/domain/models/view/AllUsersViewModel.java
@@ -0,0 +1,35 @@
+package pizzaco.domain.models.view;
+
+public class AllUsersViewModel {
+
+    private String id;
+    private String email;
+    private String fullName;
+
+    public AllUsersViewModel() {
+    }
+
+    public String getId() {
+        return this.id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getFullName() {
+        return this.fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+}
