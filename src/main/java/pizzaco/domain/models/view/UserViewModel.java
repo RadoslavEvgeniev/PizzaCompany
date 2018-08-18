ee2dd340b1a7257bdfd1409332176ae14733b2e0
Radoslav <r.evgeniev.ivanov@gmail.com>
2018-08-18 16:36:03 +0300

    Added
    UserViewModel

---
+++ src/main/java/pizzaco/domain/models/view/UserViewModel.java
@@ -0,0 +1,57 @@
+package pizzaco.domain.models.view;
+
+import java.util.List;
+
+public class UserViewModel {
+
+    private String id;
+    private String email;
+    private String firstName;
+    private String lastName;
+    private List<String> roles;
+    // TODO : Orders
+
+
+    public UserViewModel() {
+    }
+
+    public String getId() {
+        return this.id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getFirstName() {
+        return this.firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return this.lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public List<String> getRoles() {
+        return this.roles;
+    }
+
+    public void setRoles(List<String> roles) {
+        this.roles = roles;
+    }
+}
