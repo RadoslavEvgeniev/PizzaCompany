d742009f0332e94acc6fa2bd2721f57391444996
Radoslav <r.evgeniev.ivanov@gmail.com>
2018-08-18 16:29:29 +0300

    Custom query method for extracting and ordering users by email

--- src/main/java/pizzaco/repository/UserRepository.java
+++ src/main/java/pizzaco/repository/UserRepository.java
@@ -1,13 +1,18 @@
 package pizzaco.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import pizzaco.domain.entities.User;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, String> {
 
     Optional<User> findByUsername(String email);
+
+    @Query("SELECT u FROM User u order by u.username")
+    List<User> findAllOrderedAlphabetically();
 }
