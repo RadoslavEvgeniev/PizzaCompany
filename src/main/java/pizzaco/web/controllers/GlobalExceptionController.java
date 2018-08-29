7c2419c13360ffaf24926b7887b9ffdbfe258332
Radoslav <r.evgeniev.ivanov@gmail.com>
2018-08-29 15:42:35 +0300

    Fixed error template

--- src/main/java/pizzaco/web/controllers/GlobalExceptionController.java
+++ src/main/java/pizzaco/web/controllers/GlobalExceptionController.java
@@ -8,11 +8,11 @@ import org.springframework.web.servlet.ModelAndView;
 @ControllerAdvice
 public class GlobalExceptionController extends BaseController {
 
-//    @ExceptionHandler(RuntimeException.class)
-//    public ModelAndView getException(RuntimeException re) {
-//
-//        return super.view("error-template", re.getClass().isAnnotationPresent(ResponseStatus.class)
-//                ? re.getClass().getAnnotation(ResponseStatus.class).reason()
-//                : "Something went wrong");
-//    }
+    @ExceptionHandler(RuntimeException.class)
+    public ModelAndView getException(RuntimeException re) {
+
+        return super.view("error-template", "message", re.getClass().isAnnotationPresent(ResponseStatus.class)
+                ? re.getClass().getAnnotation(ResponseStatus.class).reason()
+                : "Something went wrong");
+    }
 }
